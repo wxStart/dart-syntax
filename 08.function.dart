7110b111de3ffb7d31852f404b67a52f9616153f
wangxing <982953267@qq.com>
2022-08-10 15:09:45 +0800

    [function]函数的参数功能

--- 08.function.dart
+++ 08.function.dart
@@ -19,6 +19,47 @@ void main() {
 
   // 立即执行函数
   (int n) {
-    print(n); // 9 
-  }(9); 
+    print(n); // 9
+  }(9);
+
+  // 必填参数
+  String userInfo(String name) {
+    return "你好，$name";
+  }
+
+  print(userInfo('法外狂徒'));
+
+  // 可选参数
+  String userInfo1(String name, [int? age]) {
+    return "你好，$name, 年龄：$age ";
+  }
+
+  print(userInfo1('法外狂徒'));
+  print(userInfo1('法外狂徒', 50));
+  // 命名参数
+  String userInfo2({String name = '默认名字', int? age}) {
+    return "你好，$name, 年龄：$age ";
+  }
+  print(userInfo2());
+  print(userInfo2(name: '法外狂徒', age: 50));
+
+  
+  // 函数参数 函数作为参数
+
+  // 作用域
+  var a = 1, b = 123;
+  var fn = () {
+    var a = 12;
+    print('a： $a'); // 使用的是局部作用域的变量a;
+    print('b： $b'); // 使用的是main函数中的变量b;
+  };
+  fn();
+
+  // 闭包, 函数作用域没被释放，和js一样
+  var closure = (() {
+    var a = 1;
+    return () => ++a;
+  })();
+  print(closure()); // 2
+  print(closure()); // 3
 }
